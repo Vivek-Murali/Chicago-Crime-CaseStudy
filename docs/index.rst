1ba282b8d1dac19ea9e60535a0865ac583aa747b
Vivek-Murali <vivekmurali00@outlook.com>
2024-07-01 22:19:34 +0530

    Added Project Outline

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. Chicago-Crime-CaseStudy documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Chicago-Crime-CaseStudy documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This case study is a comprehensive analysis of data crimes from 2001 to present using various data engineering tools and practices. The goal of this project is to provide insights into crime trends, patterns, and correlations using datasets from Airflow, AWS, GCP, DBT, PySpark, PyFlink, and other related technologies.',
+    author='Vivek-Murali',
+    license='MIT',
+)
